4fcbfc19ec332ef907950026362e8fe156fb3b6f
live13ruby <live13@mail.ru>
2015-11-01 08:23:35 +0200

    init

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,14 @@ import views.html.*;
 public class Application extends Controller {
 
     public Result index() {
-        return ok(index.render("Space trader."));
+        response().setContentType("text/html");
+        return ok("<h1>Hello World!</h1>");
+        //return notFound("<h1>Page not found</h1>").as("text/html");
+        //return redirect(controllers.routes.Application.hello("Bob"));
+        //return ok(index.render("Space trader 2."));
     }
+    public Result hello(String id) {
+            return ok("hello"+id);
+        }
 
 }
--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := """play-java"""
+name := """soacetrade"""
 
 version := "1.0-SNAPSHOT"
 
